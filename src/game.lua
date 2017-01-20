5c4b3a387c92a46c9c24467d98a649717d219c6e
Giseudo Oliveira <giseudo@gmail.com>
2017-01-19 21:49:47 -0300

    Subscribed to "input/key-up" message

--- src/game.lua
+++ src/game.lua
@@ -5,12 +5,16 @@ local Game = Class{
 }
 
 function Game:init(bus)
-	print("Init: Game")
 	self.bus = bus
+
+	self.bus:subscribe("input/key-up", function(payload) self:onKeyUp(payload) end)
 end
 
 function Game:update(dt)
-	print("Update: Game")
+end
+
+function Game:onKeyUp(payload)
+	print("Player pressed: " .. payload)
 end
 
 return Game

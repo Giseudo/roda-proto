87ef45b164245984f4bef38efb5d70bae23cca80
Giseudo Oliveira <giseudo@gmail.com>
2017-02-19 12:46:23 -0300

    Created first button

--- src/game.lua
+++ src/game.lua
@@ -1,30 +1,36 @@
 local Class = require (LIB_PATH .. "hump.class")
+local Vector = require (LIB_PATH .. "hump.vector")
+local Button = require (RODA_PATH .. "gui.button")
 
 local Game = Class{
-	bus = nil
+	bus = nil,
+	button = {}
 }
 
 function Game:init(bus)
 	self.bus = bus
 
-	self.bus:subscribe("update", function(event) self:update(event) end)
-	self.bus:subscribe("draw", function(event) self:draw(event) end)
-	self.bus:subscribe("input/key-press", function(event) self:keyPress(event) end)
-	self.bus:subscribe("input/key-up", function(event) self:keyUp(event) end)
+	self.button = Button(Vector(0, 0), 100, 20)
+
+	self.bus:subscribe("update", function(message) self:update(message) end)
+	self.bus:subscribe("camera/gui/draw", function(message) self:draw(message) end)
+	self.bus:subscribe("input/key-press", function(message) self:keyPress(message) end)
+	self.bus:subscribe("input/key-up", function(message) self:keyUp(message) end)
 end
 
-function Game:update(event)
+function Game:update(message)
 end
 
 function Game:draw()
+	self.button:draw()
 end
 
-function Game:keyPress(event)
-	print("Player pressed:" .. event.key)
+function Game:keyPress(message)
+	print("Player pressed:" .. message.key)
 end
 
-function Game:keyUp(event)
-	print("Player released:" .. event.key)
+function Game:keyUp(message)
+	print("Player released:" .. message.key)
 end
 
 return Game
